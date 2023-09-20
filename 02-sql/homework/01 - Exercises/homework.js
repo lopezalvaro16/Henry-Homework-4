8d444bbfe9480a2945ca059de23658ab337c3fd6
lopezalvaro16 <alvaro154020747@gmail.com>
2023-09-20 10:18:06 -0300

    Nuevo repo de M4 y ejercicio resuelto de sql

---
+++ 02-sql/homework/01 - Exercises/homework.js
@@ -0,0 +1,24 @@
+/* ¡Escribe tus comandos en este archivo! */
+
+const ejercicio02 = "SELECT * FROM movies WHERE duration < 90;";
+
+const ejercicio03 = "SELECT * FROM movies WHERE year BETWEEN 1930 AND 1940";
+
+const ejercicio04 = ("SELECT * FROM movies WHERE title LIKE " % til) % ";";
+
+const ejercicio05 = "SELECT * FROM movies WHERE array_length(actors,1)=1;";
+
+const ejercicio06 =
+  "SELECT title, AVG(rating) FROM movies, unnest(ratings) rating  GROUP BY title ORDER BY avg DESC;";
+
+const ejercicio07 =
+  'SELECT actors FROM movies WHERE (title LIKE "%Fast and%" AND year = 2016);';
+
+module.exports = {
+  ejercicio02,
+  ejercicio03,
+  ejercicio04,
+  ejercicio05,
+  ejercicio06,
+  ejercicio07,
+};
